729bc1ef3acd2c69e41b2ae9b594daa5a3f6959d
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-27 19:22:24 +0300

    Added a code friemdly for markdown

--- app/main/views.py
+++ app/main/views.py
@@ -26,3 +26,14 @@ def new_blog():
 
     title="Write a blog"
     return render_template("new_blog.html",title=title,newBlog=form)
+
+@main.route("/read_blog/title/<int:id>")
+def read_blog(id):
+    title="Blog"
+    message="Welcome to my Blog"
+    blogs=Blogs.query.all()
+    form = CommentForm()
+
+
+    format_blog=markdown2.markdown(blogs[id-1].body,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template("read_blog.html",format_blog=format_blog,message=message,title=title,comments=form,blogs=blogs,id=id)
